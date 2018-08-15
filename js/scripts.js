8a5c60ba1579b3ef8641194335589bf2b29aa387
Hyung Lee and Regina Nurieva <regina@>
2018-08-15 09:53:38 -0700

    Add JavaScript function for single-character vowel inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,36 @@
 function translate(string) {
+  var returnString;
   if (hasNumbers(string)) {
     return string;
+  } else if (checkStarting(string) === "vowel") {
+    returnString = ifVowel(string);
+    alert(returnString);
+  } else if (checkStarting(string) === "consonant") {
+
   }
 }
 
+//Check if string has a number
 function hasNumbers(string) {
   var regex = /\d/g;
   return regex.test(string);
+}
+
+//Checks if starting character is a vowel or consonant
+function checkStarting(string) {
+  if (string.charAt(0) === "a" || string.charAt(0) === "e" || string.charAt(0) === "i" || string.charAt(0) === "o" || string.charAt(0) === "u") {
+    return "vowel"
+  } else {
+    return "consonant"
+  }
+}
+
+//Function for string starting with vowel
+function ifVowel(string) {
+  if (string.length === 1) {
+    string = string + "ay";
+    return string;
+  }
 }
 
 
